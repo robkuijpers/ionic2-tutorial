f2af83810b2f756b5c38531fa7f0f43ff4c6c1bc
Rob Kuijpers <dekuijpers@gmail.com>
2016-04-05 21:10:45 +0200

    initial commit

---
+++ ionic2-tutorial-github/app/pages/home/home.ts
@@ -0,0 +1,31 @@
+import {Page, NavController} from 'ionic-angular';  
+import {GitHubService} from '../../services/github';
+import {DetailsPage} from '../details/details'; 
+
+@Page({
+    templateUrl: 'build/pages/home/home.html',
+    providers: [GitHubService]
+})
+
+export class HomePage {  
+    public foundRepos;
+    public username;
+
+    constructor(private github: GitHubService,
+                private nav: NavController) {
+    }
+
+    getRepos() {
+        this.github.getRepos(this.username).subscribe(
+            data => {
+                this.foundRepos = data.json();
+            },
+            err => console.error(err),
+            () => console.log('getRepos completed')
+        );
+    }
+    
+    goToDetails(repo) {  
+        this.nav.push(DetailsPage, { repo: repo });
+    }
+}
